478c8b8c6346a38c599532d8c3d35dbdc6ee39c5
Louis-duc <ducrocq.louis16@gmail.com>
2021-05-20 17:03:26 +0200

    new textures and update of HUD

--- src/app/services/game/fps/game-ui.service.ts
+++ src/app/services/game/fps/game-ui.service.ts
@@ -4,6 +4,8 @@ import * as BABYLON from '@babylonjs/core';
 import * as GUI from '@babylonjs/gui';
 import { GamePlayerService } from './player/game-player.service';
 
+import { MenuService } from '../../menu/menu.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +18,7 @@ export class GameUIService {
   displayUI:Function;
   changeWeapon:Function;
 
-  constructor() {
+  constructor(menuService: MenuService) {
     this.hasShot = false;
     this.currentWeapon = new GUI.Image("weapons", "assets/textures/weapons.png");
     //width and height of the sprite
@@ -157,8 +159,9 @@ export class GameUIService {
       hudWindow8.left = "-970px";
       ui.addControl(hudWindow8);
 
+      // font color : gris #969696   rouge #9a0101
       //ammo 
-      let ammoWindow:GUI.Image = new GUI.Image("ammoWindow", "assets/hud/statusClear.png");
+      let ammoWindow:GUI.Image = new GUI.Image("ammoWindow", "assets/hud/statusCleaar.png");
       ammoWindow.width = "269px";
       ammoWindow.height = "275px";
       ammoWindow.top = "410px";
@@ -167,15 +170,33 @@ export class GameUIService {
       let ammoPercent:GUI.TextBlock = new GUI.TextBlock("ammotext", "AMMO");
       ammoPercent.width = "269px";
       ammoPercent.height = "275px";
-      ammoPercent.top = "410px";
+      ammoPercent.top = "460px";
       ammoPercent.left = "-764px";
       ammoPercent.fontFamily = "DooM";
-      ammoPercent.color = "#e10101";
-      ammoPercent.fontSize = "45px";
+      ammoPercent.color = "#969696";
+      ammoPercent.fontSize = "35px";
+      menuService.addShadow(ammoPercent);
       ui.addControl(ammoPercent);
+      let ammoAmount = !player.equipedWeapon ? "" : (
+        player.equipedWeapon == 1 || player.equipedWeapon == 4 ? player.ammos[1].toString() : (
+          player.equipedWeapon == 2 || player.equipedWeapon == 3 ? player.ammos[2].toString() :
+            player.ammos[4].toString()
+        )
+      );
+      console.log(ammoAmount, player.ammos);
+      let ammoValue:GUI.TextBlock = new GUI.TextBlock("armorValue", ammoAmount);
+      ammoValue.width = "359px";
+      ammoValue.height = "275px";
+      ammoValue.top = "350px";
+      ammoValue.left = "-764px";
+      ammoValue.fontFamily = "DooM";
+      ammoValue.color = "#9a0101";
+      ammoValue.fontSize = "80px";
+      menuService.addShadow(ammoValue);
+      ui.addControl(ammoValue);
 
       //health 
-      let healthWindow:GUI.Image = new GUI.Image("ammoWindow", "assets/hud/statusClear.png");
+      let healthWindow:GUI.Image = new GUI.Image("ammoWindow", "assets/hud/statusCleaar.png");
       healthWindow.width = "359px";
       healthWindow.height = "275px";
       healthWindow.top = "410px";
@@ -184,20 +205,53 @@ export class GameUIService {
       let healthPercent:GUI.TextBlock = new GUI.TextBlock("healthtext", "HEALTH");
       healthPercent.width = "359px";
       healthPercent.height = "275px";
-      healthPercent.top = "410px";
+      healthPercent.top = "460px";
       healthPercent.left = "-430px";
       healthPercent.fontFamily = "DooM";
-      healthPercent.color = "#e10101";
-      healthPercent.fontSize = "45px";
+      healthPercent.color = "#969696";
+      healthPercent.fontSize = "35px";
+      menuService.addShadow(healthPercent);
       ui.addControl(healthPercent);
+      let healthValue:GUI.TextBlock = new GUI.TextBlock("healthValue", player.health.toString() + "%");
+      healthValue.width = "359px";
+      healthValue.height = "275px";
+      healthValue.top = "350px";
+      healthValue.left = "-430px";
+      healthValue.fontFamily = "DooM";
+      healthValue.color = "#9a0101";
+      healthValue.fontSize = "80px";
+      menuService.addShadow(healthValue);
+      ui.addControl(healthValue);
+      let healthLine:GUI.Image = new GUI.Image("healthLine", "assets/hud/line.png");
+      healthLine.width = "20px";
+      healthLine.height = "300px";
+      healthLine.top = "410px";
+      healthLine.left = "-620px";
+      ui.addControl(healthLine);
 
       //weapon selection 
-      let weaponWindow:GUI.Image = new GUI.Image("weaponWindow", "assets/hud/statusClear.png");
-      weaponWindow.width = "179px";
+      let weaponWindow:GUI.Image = new GUI.Image("weaponWindow", "assets/hud/statusCleaar.png");
+      weaponWindow.width = "182px";
       weaponWindow.height = "275px";
       weaponWindow.top = "410px";
       weaponWindow.left = "-140px";
       ui.addControl(weaponWindow);
+      let weaponSelect:GUI.TextBlock = new GUI.TextBlock("weaponSelct", "ARMS");
+      weaponSelect.width = "182px";
+      weaponSelect.height = "275px";
+      weaponSelect.top = "460px";
+      weaponSelect.left = "-140px";
+      weaponSelect.fontFamily = "DooM";
+      weaponSelect.color = "#969696";
+      weaponSelect.fontSize = "35px";
+      menuService.addShadow(weaponSelect);
+      ui.addControl(weaponSelect);
+      let weaponLine:GUI.Image = new GUI.Image("weaponLine", "assets/hud/line.png");
+      weaponLine.width = "20px";
+      weaponLine.height = "300px";
+      weaponLine.top = "410px";
+      weaponLine.left = "-241px";
+      ui.addControl(weaponLine);
 
       //robot cam
       let robotWindow:GUI.Image = new GUI.Image("weaponWindow", "assets/hud/robotCam.png");
@@ -206,9 +260,21 @@ export class GameUIService {
       robotWindow.top = "410px";
       robotWindow.left = "60px";
       ui.addControl(robotWindow);
+      let robotLine:GUI.Image = new GUI.Image("robotLine", "assets/hud/line.png");
+      robotLine.width = "20px";
+      robotLine.height = "300px";
+      robotLine.top = "410px";
+      robotLine.left = "160px";
+      ui.addControl(robotLine);
+      let robotLine2:GUI.Image = new GUI.Image("robotLine2", "assets/hud/line.png");
+      robotLine2.width = "20px";
+      robotLine2.height = "300px";
+      robotLine2.top = "410px";
+      robotLine2.left = "-40px";
+      ui.addControl(robotLine2);
 
       //armor window
-      let armorWindow:GUI.Image = new GUI.Image("weaponWindow", "assets/hud/statusClear.png");
+      let armorWindow:GUI.Image = new GUI.Image("weaponWindow", "assets/hud/statusCleaar.png");
       armorWindow.width = "359px";
       armorWindow.height = "275px";
       armorWindow.top = "410px";
@@ -217,28 +283,85 @@ export class GameUIService {
       let armorPercent:GUI.TextBlock = new GUI.TextBlock("healthtext", "ARMOR");
       armorPercent.width = "359px";
       armorPercent.height = "275px";
-      armorPercent.top = "410px";
+      armorPercent.top = "460px";
       armorPercent.left = "350px";
       armorPercent.fontFamily = "DooM";
-      armorPercent.color = "#e10101";
-      armorPercent.fontSize = "45px";
+      armorPercent.color = "#969696";
+      armorPercent.fontSize = "35px";
+      menuService.addShadow(armorPercent);
       ui.addControl(armorPercent);
+      let armorValue:GUI.TextBlock = new GUI.TextBlock("armorValue", player.armor.toString() + "%");
+      armorValue.width = "359px";
+      armorValue.height = "275px";
+      armorValue.top = "350px";
+      armorValue.left = "350px";
+      armorValue.fontFamily = "DooM";
+      armorValue.color = "#9a0101";
+      armorValue.fontSize = "80px";
+      menuService.addShadow(armorValue);
+      ui.addControl(armorValue);
+      let armorLine:GUI.Image = new GUI.Image("armorLine", "assets/hud/line.png");
+      armorLine.width = "20px";
+      armorLine.height = "300px";
+      armorLine.top = "410px";
+      armorLine.left = "530px";
+      ui.addControl(armorLine);
 
       //inventory window
-      let inventoryWindow:GUI.Image = new GUI.Image("weaponWindow", "assets/hud/statusClear.png");
+      let inventoryWindow:GUI.Image = new GUI.Image("weaponWindow", "assets/hud/statusCleaar.png");
       inventoryWindow.width = "89px";
       inventoryWindow.height = "275px";
       inventoryWindow.top = "410px";
       inventoryWindow.left = "594px";
       ui.addControl(inventoryWindow);
+      let inventoryLine:GUI.Image = new GUI.Image("inventoryLine", "assets/hud/line.png");
+      inventoryLine.width = "20px";
+      inventoryLine.height = "300px";
+      inventoryLine.top = "410px";
+      inventoryLine.left = "649px";
+      ui.addControl(inventoryLine);
+      
 
-      //amo window
-      let amoWindow:GUI.Image = new GUI.Image("weaponWindow", "assets/hud/statusClear.png");
-      amoWindow.width = "359px";
-      amoWindow.height = "275px";
-      amoWindow.top = "410px";
-      amoWindow.left = "838px";
-      ui.addControl(amoWindow);
+      //allAmmo window
+      let allAmmoWindow:GUI.Image = new GUI.Image("weaponWindow", "assets/hud/statusCleaar.png");
+      allAmmoWindow.width = "259px";
+      allAmmoWindow.height = "275px";
+      allAmmoWindow.top = "410px";
+      allAmmoWindow.left = "783px";
+      ui.addControl(allAmmoWindow);
+      let ammoPool1:GUI.TextBlock = new GUI.TextBlock("ammoPool1", "BULL " + player.ammos[1].toString() + "/" + "200");
+      ammoPool1.width = "259px";
+      ammoPool1.height = "275px";
+      ammoPool1.top = "310px";
+      ammoPool1.left = "803px";
+      ammoPool1.fontFamily = "DooM";
+      ammoPool1.color = "#969696";
+      ammoPool1.fontSize = "20px";
+      ammoPool1.textHorizontalAlignment = GUI.TextBlock.HORIZONTAL_ALIGNMENT_LEFT;
+      menuService.addShadow(ammoPool1);
+      ui.addControl(ammoPool1);
+      let ammoPool2:GUI.TextBlock = new GUI.TextBlock("ammoPool2", "SHEL " + player.ammos[2].toString() + "/" + "50");
+      ammoPool2.width = "259px";
+      ammoPool2.height = "275px";
+      ammoPool2.top = "380px";
+      ammoPool2.left = "803px";
+      ammoPool2.fontFamily = "DooM";
+      ammoPool2.color = "#969696";
+      ammoPool2.fontSize = "20px";
+      ammoPool2.textHorizontalAlignment = GUI.TextBlock.HORIZONTAL_ALIGNMENT_LEFT;
+      menuService.addShadow(ammoPool2);
+      ui.addControl(ammoPool2);
+      let ammoPool3:GUI.TextBlock = new GUI.TextBlock("ammoPool1", "CELL " + player.ammos[4].toString() + "/" + "300");
+      ammoPool3.width = "259px";
+      ammoPool3.height = "275px";
+      ammoPool3.top = "450px";
+      ammoPool3.left = "803px";
+      ammoPool3.fontFamily = "DooM";
+      ammoPool3.color = "#969696";
+      ammoPool3.fontSize = "20px";
+      ammoPool3.textHorizontalAlignment = GUI.TextBlock.HORIZONTAL_ALIGNMENT_LEFT;
+      menuService.addShadow(ammoPool3);
+      ui.addControl(ammoPool3);
       
 
     }
