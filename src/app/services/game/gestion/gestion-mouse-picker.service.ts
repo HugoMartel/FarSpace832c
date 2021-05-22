cebc9539883bd086c15d60c7239d4d9831893529
Spac3Drunk <louis.manouvriez@gmail.com>
2021-05-22 16:37:27 +0200

    Buildings placable git add .!                                        coulor green/red ok with terrain, just need to care about other building

--- src/app/services/game/gestion/gestion-mouse-picker.service.ts
+++ src/app/services/game/gestion/gestion-mouse-picker.service.ts
@@ -7,6 +7,10 @@ import { GestionMeshLoaderService } from './gestion-mesh-loader.service';
 })
 export class GestionMousePickerService {
 
+  private isPlacable: boolean = false;
+  private lastPosX: number = 0;
+  private lastPosY: number = 0;
+
   constructor(private gesMeLoadService: GestionMeshLoaderService) { }
 
   public addMouseListener(scene:BABYLON.Scene, matrix: any[]) {
@@ -16,38 +20,44 @@ export class GestionMousePickerService {
       // Detect the event type and if the input is a left click
       //! BABYLON.PointerInput.LeftClick means right click...)
       if (ptInfo.type === BABYLON.PointerEventTypes.POINTERMOVE) {
+        this.isPlacable = false;
         let pickInfo:BABYLON.PickingInfo|null = scene.pick(scene.pointerX, scene.pointerY, undefined);
-        if (pickInfo !== null && pickInfo.hit && pickInfo.pickedMesh !== null) {
-          //console.log(pickInfo.pickedMesh.position.y*2);
-          if (pickInfo.pickedMesh.metadata == "ground") {
+        if (pickInfo !== null && pickInfo.hit && pickInfo.pickedMesh !== null && pickInfo.pickedMesh.metadata == "ground") {
 
-            let pasContent = new BABYLON.StandardMaterial("pasContent", scene);
-            pasContent.ambientColor = new BABYLON.Color3(1, 0, 0);
-            let content = new BABYLON.StandardMaterial("content", scene);
-            content.ambientColor = new BABYLON.Color3(0, 1, 0);
+          this.lastPosX = pickInfo.pickedMesh.position.x;
+          this.lastPosY = pickInfo.pickedMesh.position.z;
 
+          this.gesMeLoadService.baseMeshes[0].position.x = this.lastPosX;
+          this.gesMeLoadService.baseMeshes[0].position.z = this.lastPosY;
+          this.gesMeLoadService.baseMeshes[0].position.y = matrix[this.lastPosX][this.lastPosY]+0.05;
 
-            this.gesMeLoadService.baseMeshes[0].position.x = pickInfo.pickedMesh.position.x;
-            this.gesMeLoadService.baseMeshes[0].position.z = pickInfo.pickedMesh.position.z;
-            this.gesMeLoadService.baseMeshes[0].position.y = matrix[pickInfo.pickedMesh.position.x][pickInfo.pickedMesh.position.z]+0.05;
-            this.gesMeLoadService.baseMeshes[0].getChildMeshes().forEach((element: any) => {
-              element.isVisible = true;
-              element.material = content;
-            });
-
-          }else {
-            this.gesMeLoadService.baseMeshes[0].getChildMeshes().forEach((element: any) => {
-              element.isVisible = false;
-            });
+          this.isPlacable = true;
+          for (let x = -1; x < 2; x++) {
+            for (let y = -1; y < 2; y++) {
+              if (matrix[this.lastPosX][this.lastPosY] != matrix[this.lastPosX+x][this.lastPosY+y]) {
+                this.isPlacable = false;
+              }
+            }
           }
-        }else {
-          this.gesMeLoadService.baseMeshes[0].getChildMeshes().forEach((element: any) => {
-            element.isVisible = false;
-          });
-        }
-      } else if (ptInfo.type === BABYLON.PointerEventTypes.POINTERUP && ptInfo.event.button === 0) {
 
+          let placableColor = new BABYLON.StandardMaterial("isPlacable", scene);
+          this.isPlacable ? placableColor.ambientColor = new BABYLON.Color3(0, 1, 0) : placableColor.ambientColor = new BABYLON.Color3(1, 0, 0);
+
+          this.gesMeLoadService.baseMeshes[0].getChildMeshes().forEach((element: any) => {
+            element.isVisible = true;
+            element.material = placableColor;
+          });
+      }else {
+        this.gesMeLoadService.baseMeshes[0].getChildMeshes().forEach((element: any) => {
+          element.isVisible = false;
+        });
       }
+    } else if (ptInfo.type === BABYLON.PointerEventTypes.POINTERUP && ptInfo.event.button === 0) {
+        if (this.isPlacable) {
+          this.gesMeLoadService.load1stQG(this.lastPosX, this.lastPosY, scene, matrix);
+          this.isPlacable = false;
+        }
+      }else {}
     });
   }
 
