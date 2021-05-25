025885fe9367ba15b146911bd960935c8fcc7fdc
Hugo Martel <hugo.martel@student.yncrea.fr>
2021-05-26 00:03:29 +0200

    Added the slides and functions associated with it Also added the logo and fixed a font bug that we had encountered

--- src/app/services/game/gestion/gestion-slides.service.ts
+++ src/app/services/game/gestion/gestion-slides.service.ts
@@ -8,8 +8,13 @@ import * as GUI from '@babylonjs/gui';
 })
 export class GestionSlidesService {
   private slides: GUI.Image[] = [];
+  private currentSlide: number;
+  private playButton:GUI.Button;
 
-  constructor(@Inject(Number)index: number) { 
+  constructor(@Inject(Number) index: number, buttonCallback:Function) {
+
+    this.currentSlide = 0;
+
     switch (index) {
       // Energy
       case 0:
@@ -50,6 +55,21 @@ export class GestionSlidesService {
         //TODO
         break;
     }
+
+
+    this.playButton = GUI.Button.CreateImageWithCenterTextButton("playButton", "Find the resource crate!", "assets/textures/menu/buttonGradient.png");
+    this.playButton.width = "400px";
+    this.playButton.height = "40px";
+    this.playButton.color = "white";
+    this.playButton.fontFamily = "DooM";
+    this.playButton.background = "green";
+    this.playButton.shadowBlur = 1;
+    this.playButton.shadowColor = "black";
+    this.playButton.shadowOffsetX = 1;
+    this.playButton.shadowOffsetY = 1;
+    this.playButton.onPointerClickObservable.add(() => {
+      buttonCallback();
+    });
   }
 
 
@@ -66,18 +86,44 @@ export class GestionSlidesService {
 
   /**
    * Function to display the information about the last placed module
+   * @param scene Babylon scene to display the slides on
    * @param hud HUD to fill the images in
    */
   public displaySlides(scene:BABYLON.Scene, hud:GUI.AdvancedDynamicTexture) {
     // Keyboard event to detect a next slide call
-    let keyboardEvent = () => {
-
+    let keyboardEvent = (kbInfo:BABYLON.KeyboardInfo) => {
+      kbInfo.event.preventDefault();
+      switch (kbInfo.type) {
+        /* One time trigger events */
+        case BABYLON.KeyboardEventTypes.KEYUP:
+          switch (kbInfo.event.code) {
+            case "ArrowRight":
+              // go to the next slide
+              if (this.currentSlide + 1 < this.slides.length) {
+                hud.addControl(this.slides[++this.currentSlide]);
+                hud.removeControl(this.slides[this.currentSlide - 1]);
+              // if the last slide was reached display a button
+              } else if (this.currentSlide + 1 == this.slides.length) {
+                hud.addControl(this.playButton);
+              }
+              break;
+            case "ArrowLeft":
+              // go to the prev slide
+              if (this.currentSlide > 0) {
+                hud.addControl(this.slides[--this.currentSlide]);
+                hud.removeControl(this.slides[this.currentSlide + 1]);
+              // go to the last slide and remove button
+              } else if (this.currentSlide == this.slides.length) {
+                hud.addControl(this.slides[--this.currentSlide]);
+                hud.removeControl(this.playButton);
+              }
+              break;
+          }
+      }
     };
 
     /* Add the keyboard events */
     scene.onKeyboardObservable.add(keyboardEvent);
-
-    //TODO
 
   }
 }
